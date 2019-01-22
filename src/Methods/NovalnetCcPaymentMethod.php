0154346890bae18e57e9073bd03849fd662c5121
kotteeswaran <kotteeswaranviit@gmail.com>
2019-01-22 16:34:42 +0530

    Update NovalnetCcPaymentMethod.php

--- src/Methods/NovalnetCcPaymentMethod.php
+++ src/Methods/NovalnetCcPaymentMethod.php
@@ -82,9 +82,14 @@ class NovalnetCcPaymentMethod extends PaymentMethodService
      */
     public function getIcon():string
     {
-        /** @var Application $app */
-        $app = pluginApp(Application::class);
-        return $app->getUrlPath('novalnet') .'/images/cc.png';
+        $logoUrl = $this->configRepository->get('Novalnet.novalnet_cc_payment_logo');
+        if($logoUrl == 'images/cc.png'){
+            /** @var Application $app */
+            $app = pluginApp(Application::class);
+            $logoUrl = $app->getUrlPath('novalnet') .'/images/cc.png';
+        } 
+        return $logoUrl;
+        
     }
 
     /**
