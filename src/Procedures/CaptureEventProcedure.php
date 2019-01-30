c191ec75e5782684adb1c7579c2eeac86ed7c34c
kotteeswaran <kotteeswaranviit@gmail.com>
2019-01-30 11:28:47 +0530

    Update CaptureEventProcedure.php

--- src/Procedures/CaptureEventProcedure.php
+++ src/Procedures/CaptureEventProcedure.php
@@ -6,6 +6,7 @@ use Plenty\Modules\EventProcedures\Events\EventProceduresTriggered;
 use Plenty\Modules\Order\Models\Order;
 use Plenty\Plugin\Log\Loggable;
 use Novalnet\Helper\PaymentHelper;
+use Plenty\Modules\Payment\Contracts\PaymentRepositoryContract;
 
 /**
  * Class CaptureEventProcedure
@@ -13,11 +14,13 @@ use Novalnet\Helper\PaymentHelper;
 class CaptureEventProcedure
 {
 	use Loggable;
-	private $paymentHelper;
-    public function __construct(PaymentHelper $paymentHelper)
-    {
-        $this->paymentHelper            = $paymentHelper;
-    }	
+	
+	//private $paymentHelper;
+    //public function __construct(PaymentHelper $paymentHelper)
+    //{
+        //$this->paymentHelper            = $paymentHelper;
+    //}	
+	
     /**
      * @param EventProceduresTriggered $eventTriggered
      * @param Capture $captureService
@@ -26,9 +29,15 @@ class CaptureEventProcedure
         EventProceduresTriggered $eventTriggered
     ) {
         /* @var $order Order */
-	   
-        $order = $eventTriggered->getOrder(); 
-	$this->paymentHelper->payments($order->id);    
+	 
+	    $order = $eventTriggered->getOrder(); 
+	    
+	    $payment = pluginApp(\Plenty\Modules\Payment\Contracts\PaymentRepositoryContract::class);  
+       	    $details = $payment->getPaymentsByOrderId( $order->id);
+	    $this->getLogger(__METHOD__)->error('456',$payment );
+	    $this->getLogger(__METHOD__)->error('789',$details );
+	//$this->paymentHelper->payments($order->id);   
+	    
         $this->getLogger(__METHOD__)->error('EventProcedure.triggerFunction', ['order' => $order]);
         //$captureService->doCapture($order);
     }
