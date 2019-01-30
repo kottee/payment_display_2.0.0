ea5041870504948d009c6395ccc22ab969d470ae
kotteeswaran <kotteeswaranviit@gmail.com>
2019-01-30 17:29:05 +0530

    Update CaptureEventProcedure.php

--- src/Procedures/CaptureEventProcedure.php
+++ src/Procedures/CaptureEventProcedure.php
@@ -41,7 +41,7 @@ class CaptureEventProcedure
 	    $this->getLogger(__METHOD__)->error('details',$paymentDetails);
 	    $key = $paymentDetails[0]->method->paymentKey;
 	    $key1 = [0]->method->paymentKey;
-	    $key2 = $paymentDetails->0->method->paymentKey;
+	   
 	    
 	    foreach ($paymentDetails as $paymentDetail)
 		{
@@ -64,8 +64,7 @@ class CaptureEventProcedure
 	     $this->getLogger(__METHOD__)->error('status',$status);
 	     $this->getLogger(__METHOD__)->error('key',$key);
 	     $this->getLogger(__METHOD__)->error('key1',$key1);
-	     $this->getLogger(__METHOD__)->error('key2',$key2);
-	
+	    
 	    
 	   
         $this->getLogger(__METHOD__)->error('EventProcedure.triggerFunction', ['order' => $order]);
